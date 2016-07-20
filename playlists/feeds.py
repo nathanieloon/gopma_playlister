b7dd04e5c4ded216c404aa34ad326b43c9ba35b9
Nathaniel Oon <nathanieloon@gmail.com>
2016-07-20 16:44:05 +1000

    Improved RSS feed messages.

--- playlists/feeds.py
+++ playlists/feeds.py
@@ -1,18 +1,19 @@
 from django.contrib.syndication.views import Feed
 from .models import Song
+from gmusicfunctions import PLAYLIST_PREFIX
 
 class LatestSongs(Feed):
-    title = "GOPMA Songs"
+    title = PLAYLIST_PREFIX + "Feed"
     link = "/"
 
     def items(self):
         return Song.objects.order_by('-date_added')
 
     def item_title(self, item):
-        return item.title
+        return item.title + " by " + item.artist
 
     def item_description(self, item):
-        return "test description"
+        return "The genre of this song is " + item.genre + "."
 
     def item_link(self, item):
         return "https://play.google.com/music/m/" + item.tid
